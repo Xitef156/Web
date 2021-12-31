ae8efdedbf46d3cb50731faa3107060b52020d1f
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2021-12-31 21:00:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express()
 const port = 8080
 
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`)})
+
 var mc = require('minecraft-protocol');
 var server = mc.createServer({
   'online-mode': true,   // optional
