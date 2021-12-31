518db4f5af4309a6c75095a6b02ad8055051c113
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2021-12-31 20:58:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
 const express = require('express');
-const fs = require('fs');
-var path = require("path");
 const app = express()
 const port = 8080
 
-const Discord = require('discord.js');
-const Instent = Discord.Intents.FLAGS
-const Client = new Discord.Client({ intents: [
-  Instent.GUILDS,
-  Instent.GUILD_MESSAGES,
-  Instent.GUILD_MESSAGE_REACTIONS,
-  Instent.GUILD_MESSAGE_TYPING,
-  Instent.DIRECT_MESSAGES,
-  Instent.DIRECT_MESSAGE_REACTIONS,
-  Instent.DIRECT_MESSAGE_TYPING,
-  Instent.GUILD_MEMBERS,
-  Instent.GUILD_VOICE_STATES,
-  Instent.GUILD_BANS,
-  Instent.GUILD_EMOJIS_AND_STICKERS,
-  Instent.GUILD_INVITES,
-  Instent.GUILD_INTEGRATIONS,
-  Instent.GUILD_WEBHOOKS
-],
-makeCache: Discord.Options.cacheWithLimits({
-  MessageManager: 200, // This is default
-  PresenceManager: 1,
-  // Add more class names here
-}), allowedMentions: { parse: ['users', 'roles', 'everyone'], repliedUser: true }
+var mc = require('minecraft-protocol');
+var server = mc.createServer({
+  'online-mode': true,   // optional
+  encryption: true,      // optional
+  host: '0.0.0.0',       // optional
+  port: 25565,           // optional
+  version: '1.18.1'
 });
-
-app.use(express.static('img'));
-var absolutePath = path.resolve('./test.png');
-app.get('/',function(req,res){
-  Client.channels.cache.get(922507312181174342).send(`${req.path} ; ${req.params} ; ${req.method} ; ${req.ips} ; ${req.ip} ; ${req.httpVersion} ; ${req.hostname} ; ${req.cookies} ; ${req.app} ; ${req.accepted}`)
-  res.sendFile(absolutePath)
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
-
-Client.login(process.env.TOKEN);
+const mcData = require('minecraft-data')(server.version)
