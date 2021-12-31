bfad36795d2b0b02e3ab50f78704bfa73ef564dc
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2021-12-31 21:21:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ var mc = require('minecraft-protocol');
 var server = mc.createServer({
   'online-mode': true,   // optional
   encryption: true,      // optional
-  host: '0.0.0.0',       // optional
-  port: 25565,           // optional
   version: '1.18.1'
 });
 const mcData = require('minecraft-data')(server.version)
