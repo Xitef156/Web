ebf57d1a706940b55ed5546dd169ce96709b5742
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2022-01-16 14:52:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,8 @@ var path = require("path");
 var file = './index.html'
 app.use(express.static(__dirname));
 app.get('/', (req, res, next) => {
-  if (req.hostname === 'xitef156.tk') {
-    return next();
-  }
-
-  const user = users.find(user => user.domain === req.hostname);
-  if (!user) {
-    return res.sendStatus(404);
-  }
-  
-  return res.send(path.resolve(file));
+  res.send(path.resolve(file));
 });
-})
 
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`)})
