f52b45025d08583a502b8c0f2bfcc76cf0513c5d
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2022-01-16 13:50:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express()
 const port = 8080
+var path = require("path");
 
 
-app.get('/', (req, res) => {
-res.send('Hello World!')
+var file = './index.html'
+app.use(express.static(__dirname));
+app.get('/', (req, res, next) => {
+  if (req.hostname === 'xitef156.tk') {
+    return next();
+  }
+
+  const user = users.find(user => user.domain === req.hostname);
+  if (!user) {
+    return res.sendStatus(404);
+  }
+  
+  return res.send(path.resolve(file));
+});
 })
 
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`)})
-
-var mc = require('minecraft-protocol');
-var server = mc.createServer({
-  'online-mode': true,   // optional
-  encryption: true,      // optional
-  version: '1.18.1'
-});
-const mcData = require('minecraft-data')(server.version)
